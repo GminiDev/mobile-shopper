b9a291d4441168d9ebd8d432536829c56e671422
suphakorn <gan.suphakorn17@gmail.com>
2021-03-09 21:19:12 +0700

    PHP Partial Template

---
+++ cart.php
@@ -0,0 +1,21 @@
+<?php
+    //include header.php file
+    include('header.php');
+?>
+
+<?php
+
+    /* include Cart */
+    include('Template/_cart.php');
+    /* include Cart */
+
+    /* include New phones section */
+    include('Template/_new-phones.php');
+    /* include New phones section */
+
+?>
+
+<?php
+    //include footer.php file
+    include('footer.php');
+?>
---
+++ product.php
@@ -0,0 +1,21 @@
+<?php
+    //include header.php file
+    include('header.php');
+?>
+
+<?php
+
+    /* include Products */
+    include('Template/_products.php');
+    /* include Products */
+
+    /* include Top sale section */
+    include('Template/_top-sale.php');
+    /* include Top sale section */
+
+?>
+
+<?php
+    //include footer.php file
+    include('footer.php');
+?>
