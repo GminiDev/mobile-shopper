96234fc0a36102c2f1e78a4b01a5f6e930e16a89
suphakorn <gan.suphakorn17@gmail.com>
2021-03-11 13:05:39 +0700

    Wishlist

--- cart.php
+++ cart.php
@@ -9,6 +9,10 @@
     include('Template/_cart-template.php');
     /* include Cart */
 
+    /* include Wishilist */
+    include('Template/_wishilist_template.php');
+    /* include Wishilist */
+
     /* include New phones section */
     include('Template/_new-phones.php');
     /* include New phones section */
